b2eeb63d48c7a7f428b6743d47d082b6fe07eddb
xuanchiengigido <21130298@st.hcmuaf.edu.vn>
2023-10-16 08:06:42 +0700

    task 5

--- thuchanhtuanba/src/NodeComparator.java
+++ thuchanhtuanba/src/NodeComparator.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class NodeComparator {
+import java.util.Comparator;
+
+public class NodeComparator implements Comparator<Node> {
+
+    @Override
+    public int compare(Node o1, Node o2) {
+        int result = Double.compare(o1.getPathCost(), o2.getPathCost());
+        if (result != 0) return result;
+        return o1.compareTo(o2);
+    }
 }
--- thuchanhtuanba/src/UniformCostSearchAlgo.java
+++ thuchanhtuanba/src/UniformCostSearchAlgo.java
@@ -1,2 +1,97 @@
-package PACKAGE_NAME;public class UniformCostSearchAlgo {
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class UniformCostSearchAlgo implements ISearchAlgo {
+    @Override
+    public Node execute(Node root, String goal) {
+        PriorityQueue<Node> frontiers = new PriorityQueue<Node>(new NodeComparator());
+        ArrayList<Node> explored = new ArrayList<>();
+        frontiers.offer(root);
+        root.setPathCost(0);
+        while (!frontiers.isEmpty()) {
+            Node current = frontiers.poll();
+            if (current.getLabel().equals(goal)) return current;
+            explored.add(current);
+            for (Edge e :
+                    current.getChildren()) {
+                Node n = e.getEnd();
+                if (!frontiers.contains(n) && !explored.contains(n)) {
+                    n.setParent(current);
+                    n.setPathCost(current.getPathCost() + e.getWeight());
+                    frontiers.add(n);
+                } else if (frontiers.contains(n) && n.getPathCost() > (current.getPathCost() + e.getWeight())) {
+                    n.setPathCost(current.getPathCost() + e.getWeight());
+                    n.setParent(current);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Node execute(Node root, String start, String goal) {
+        PriorityQueue<Node> frontiers = new PriorityQueue<Node>(new NodeComparator());
+        ArrayList<Node> explored = new ArrayList<>();
+        frontiers.offer(root);
+        boolean flag = false;
+        root.setPathCost(0);
+        while (!frontiers.isEmpty()) {
+            Node current = frontiers.poll();
+            if(current.getLabel().equals(start) && !flag){
+                flag = true;
+                for (Node e:
+                     frontiers) {
+                    e.setPathCost(0);
+                }
+                frontiers.clear();
+                for (Node e:
+                     explored) {
+                    e.setPathCost(0);
+                }
+                explored.clear();
+                current.setParent(null);
+                frontiers.offer(current);
+            }
+            if (current.getLabel().equals(goal)) return current;
+            explored.add(current);
+            for (Edge e :
+                    current.getChildren()) {
+                Node n = e.getEnd();
+                if (!frontiers.contains(n) && !explored.contains(n)) {
+                    n.setParent(current);
+                    n.setPathCost(current.getPathCost() + e.getWeight());
+                    frontiers.add(n);
+                } else if (frontiers.contains(n) && n.getPathCost() > (current.getPathCost() + e.getWeight())) {
+                    n.setPathCost(current.getPathCost() + e.getWeight());
+                    n.setParent(current);
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Node nodeS = new Node("S");
+        Node nodeA = new Node("A");
+        Node nodeB = new Node("B");
+        Node nodeC = new Node("C");
+        Node nodeD = new Node("D");
+        Node nodeE = new Node("E");
+        Node nodeF = new Node("F");
+        Node nodeG = new Node("G");
+        Node nodeH = new Node("H");
+        nodeS.addEdge(nodeA, 5);
+        nodeS.addEdge(nodeB, 2);
+        nodeS.addEdge(nodeC, 4);
+        nodeA.addEdge(nodeD, 9);
+        nodeA.addEdge(nodeE, 4);
+        nodeB.addEdge(nodeG, 6);
+        nodeC.addEdge(nodeF, 2);
+        nodeD.addEdge(nodeH, 7);
+        nodeE.addEdge(nodeG, 6);
+        nodeF.addEdge(nodeG, 1);
+        ISearchAlgo algo1 = new UniformCostSearchAlgo();
+        Node result = algo1.execute(nodeS,"A", "G");
+        System.out.println("UCS: " + NodeUtils.printPath(result));
+    }
 }
